e1dc13b06a76ffb1db76fc6c5c3c3fe734529a5e
Yongo David <davidyongo9@gmail.com>
2024-10-20 14:42:32 +0100

    form update

--- src/Sections/Contact/index.js
+++ src/Sections/Contact/index.js
@@ -15,7 +15,7 @@ const Contact = () => {
     return(
       <Container py={2} centerContent maxHeight="container.xl" p={0}>
         <VStack py={4}><Heading size='md'>Reach out to us here</Heading></VStack>
-        <Flex height='100vh' py={20}>
+        <Flex py={10}>
           <VStack 
           w='full'
           h='full'
@@ -23,18 +23,18 @@ const Contact = () => {
           spacing={10}
           bg="gray.50"
           >
-            <SimpleGrid column={2}  columnGap={50} w="full">
+            <SimpleGrid column={2}  columnGap={50} rowGap={4}>
               <GridItem colSpan={1}>
                 <FormControl>
                   <FormLabel>Name</FormLabel>
-                  <Input size='sm'></Input>
+                  <Input size='sm' placeholder="John"></Input>
                 </FormControl>
               </GridItem>
 
               <GridItem colSpan={1}>
                 <FormControl>
                   <FormLabel>Email</FormLabel>
-                  <Input size='sm'></Input>
+                  <Input size='sm' placeholder="@email.com"></Input>
                 </FormControl>
               </GridItem>
 
@@ -43,7 +43,10 @@ const Contact = () => {
               </GridItem>
 
               <GridItem colSpan={2}>
-                <Button variant='outline' w='full'>Submit</Button>
+                <Button variant='outline' w='full' sx={{
+                  color: 'black',
+                  bg: 'red'
+                }}>Submit</Button>
               </GridItem>
             </SimpleGrid>
           </VStack>
